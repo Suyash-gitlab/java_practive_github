567826ffca637856a796d47fa317c157a41698d1
suyash <suyashgit24@gmail.com>
2025-02-03 17:48:58 +0530

    commit

---
+++ AAA_java_github/src/Newjava/Second.java
@@ -0,0 +1,17 @@
+package Newjava;
+
+public class Second {
+
+	public static void main(String[] args) {
+        
+	    
+	    int num=20,sum=0;
+
+	    for(int i=1;i<=num;i++)
+	    {
+	        sum+=i;
+	    }
+	    System.out.println(sum);
+	}
+
+}
